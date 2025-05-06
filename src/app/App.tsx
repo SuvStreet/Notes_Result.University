f7e2aaff8da626864c26dfd17ad73dccc829266b
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-05-06 14:58:22 +0300

    feat(provider): Подключить mantine.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,15 @@
 import { Router } from './router'
+import { MantineStyleProvider } from './provider'
 
 import './App.css'
 
 function App() {
-
   return (
-    <Router>
-      <h1>Notes (React + Vite + TS)</h1>
-    </Router>
+    <MantineStyleProvider>
+      <Router>
+        <h1>Notes (React + Vite + TS)</h1>
+      </Router>
+    </MantineStyleProvider>
   )
 }
 
