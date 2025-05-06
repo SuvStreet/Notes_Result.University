bdb4b076a2447c828e657eba7750f3035128532f
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-05-06 22:20:53 +0300

    feat(dev): Принимать данные из формы логина.

--- src/features/auth/ui/LoginForm.tsx
+++ src/features/auth/ui/LoginForm.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router'
+import { useForm } from '@mantine/form'
 import {
   Anchor,
   Button,
@@ -10,20 +11,48 @@ import {
 import { routePaths } from '@shared/config/routePaths'
 
 export const LoginForm = () => {
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      email: '',
+      password: '',
+    },
+
+    validate: {
+      email: (value) =>
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) ? null : 'Некорректный email',
+      password: (value) => {
+        if (!value) return 'Пароль не может быть пустым'
+        if (!/^\d+$/.test(value)) return 'Пароль должен быть из цифр'
+        if (value.length > 5)
+          return 'Пароль слишком длинный (максимум 5 символов)'
+        if (value.length < 3)
+          return 'Пароль слишком короткий (минимум 3 символа)'
+      },
+    },
+  })
+
+  const handleSubmit = (values: typeof form.values) => {
+    console.log(values)
+    form.reset()
+  }
+
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form onSubmit={form.onSubmit(handleSubmit)}>
       <Flex direction="column" gap="md">
         <TextInput
           label="Логин"
           placeholder="Введите логин"
           withAsterisk
-          required
+          key={form.key('email')}
+          {...form.getInputProps('email')}
         />
         <PasswordInput
           label="Пароль"
           placeholder="Введите пароль"
           withAsterisk
-          required
+          key={form.key('password')}
+          {...form.getInputProps('password')}
         />
       </Flex>
       <Group justify="flex-end" mt="md" align="end">
