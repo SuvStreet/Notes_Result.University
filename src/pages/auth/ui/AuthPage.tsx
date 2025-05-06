cd5b11d411c170dd83fb799a6ef09c5a24caa5f5
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-05-06 21:27:15 +0300

    feat(dev): Добавить форму авторизации.

--- src/pages/auth/ui/AuthPage.tsx
+++ src/pages/auth/ui/AuthPage.tsx
@@ -1,7 +1,10 @@
+import { LoginForm } from '@features/auth'
+
 export const AuthPage = () => {
   return (
     <div>
-      <h1>AuthPage</h1>
+      <h1>Авторизация</h1>
+      <LoginForm />
     </div>
   )
 }
