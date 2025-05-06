a493af69d4efd1f9df3367e2914211fed7202606
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-05-06 17:06:51 +0300

    refactor(alias): Добавить alias для путей.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': '/src/app',
+      '@pages': '/src/pages',
+      '@shared': '/src/shared',
+      '@features': '/src/features',
+    },
+  },
   server: {
     open: true,
   },
