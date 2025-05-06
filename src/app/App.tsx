c9872d7a4f32794160c7fb23ae92b4e67c3f23b3
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-05-06 14:49:56 +0300

    feat(app, router): Подключить роутер.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
+import { Router } from './router'
+
 import './App.css'
 
 function App() {
 
   return (
-    <>
+    <Router>
       <h1>Notes (React + Vite + TS)</h1>
-    </>
+    </Router>
   )
 }
 
