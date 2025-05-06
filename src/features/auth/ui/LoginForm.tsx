c459d58983490771922143120026cec7943d8311
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-05-06 21:27:15 +0300

    feat(dev): Добавить форму авторизации.

--- src/features/auth/ui/LoginForm.tsx
+++ src/features/auth/ui/LoginForm.tsx
@@ -1,7 +1,37 @@
+import { Link } from 'react-router'
+import {
+  Anchor,
+  Button,
+  Flex,
+  Group,
+  PasswordInput,
+  TextInput,
+} from '@mantine/core'
+import { routePaths } from '@shared/config/routePaths'
+
 export const LoginForm = () => {
   return (
-    <div>
-      <h1>Login Form</h1>
-    </div>
+    <form onSubmit={(e) => e.preventDefault()}>
+      <Flex direction="column" gap="md">
+        <TextInput
+          label="Логин"
+          placeholder="Введите логин"
+          withAsterisk
+          required
+        />
+        <PasswordInput
+          label="Пароль"
+          placeholder="Введите пароль"
+          withAsterisk
+          required
+        />
+      </Flex>
+      <Group justify="flex-end" mt="md" align="end">
+        <Anchor component={Link} to={routePaths.register} size="sm" c="grey">
+          У вас нет аккаунта? Зарегистрируйтесь
+        </Anchor>
+        <Button type="submit">Авторизоваться</Button>
+      </Group>
+    </form>
   )
 }
