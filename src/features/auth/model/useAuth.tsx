4edd5310f1547bb3247c8222b442a87cc890d39a
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-05-07 11:15:31 +0300

    feat(dev): Добавить возможность регистрации, подправить стили.

--- src/features/auth/model/useAuth.tsx
+++ src/features/auth/model/useAuth.tsx
@@ -1,1 +1,43 @@
-export const useAuth = () => {}
+import { auth } from '@shared/api/firebase'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { useState } from 'react'
+
+interface User {
+  id: string
+  email: string
+}
+
+export const useAuth = () => {
+  const [user, setUser] = useState<User | null>(null)
+  const [loading, setLoading] = useState(false)
+
+  const signUp = async (email: string, password: string) => {
+    setLoading(true)
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      const user = userCredential.user
+
+      return { user, error: null }
+
+    } catch (err) {
+      console.error('Ошибка при регистрации:', err)
+
+      return {
+        user: null,
+        error: 'Регистрация не удалась. Попробуйте еще раз.',
+      }
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return {
+    signUp,
+    loading
+  }
+}
