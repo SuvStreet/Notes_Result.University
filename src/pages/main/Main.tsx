db98ddf95998f0d824f53172ed0e907374cea616
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-05-07 14:40:15 +0300

    feat(dev): Добавить выход из аккаунта.

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,7 +1,14 @@
+import { Button, Flex } from '@mantine/core'
+
+import { useAuthContext } from '@app/provider'
+
 export const Main = () => {
+  const { signOut } = useAuthContext()
+
   return (
-    <div>
+    <Flex direction="column" align="center" justify="center" h="100vh">
       <h1>Main Page</h1>
-    </div>
+      <Button onClick={signOut}>Выход</Button>
+    </Flex>
   )
 }
