b1a216bd6092a3f2efe531ead8c218eff681586f
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-05-07 14:59:06 +0300

    refactor(auth): Рафактор AuthProvider и useAuth

--- src/app/provider/AuthProvider.tsx
+++ src/app/provider/AuthProvider.tsx
@@ -1,34 +1,12 @@
 import { createContext, useContext, type JSX } from 'react'
-import { type User } from '@features/auth/model/useAuth'
-import { useAuth } from '@features/auth/model/useAuth'
-
-interface AuthResult {
-  user: User | null
-  error: string | null
-}
-
-type AuthFn = (email: string, password: string) => Promise<AuthResult>
-
-interface AuthContextProps {
-  user: User | null
-  loading: boolean
-  initializing: boolean
-  signIn: AuthFn
-  signUp: AuthFn,
-  signOut: () => Promise<void>
-}
+import {
+  useAuth,
+  type AuthContextProps,
+  defaultAuthContext,
+} from '@features/auth/model/useAuth'
 
 interface ChildrenProps {
   children: JSX.Element
-}
-
-const defaultAuthContext: AuthContextProps = {
-  user: null,
-  loading: true,
-  initializing: true,
-  signIn: async () => ({ user: null, error: null }),
-  signUp: async () => ({ user: null, error: null }),
-  signOut: async () => {},
 }
 
 const AuthContext = createContext<AuthContextProps>(defaultAuthContext)
@@ -38,11 +16,7 @@ export function useAuthContext() {
 }
 
 export function AuthProvider({ children }: ChildrenProps) {
-  const { user, loading, initializing, signIn, signUp, signOut } = useAuth()
+  const auth = useAuth()
 
-  return (
-    <AuthContext value={{ user, loading, initializing, signIn, signUp, signOut }}>
-      {children}
-    </AuthContext>
-  )
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>
 }
