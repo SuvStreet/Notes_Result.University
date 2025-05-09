b33a9e3062f395434b5d027cf27d6f13afc99038
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-05-09 21:04:36 +0300

    feat(dev): Добавление заметок, и её редактирование.

--- src/widgets/note-list/ui/NoteList.tsx
+++ src/widgets/note-list/ui/NoteList.tsx
@@ -1,14 +1,53 @@
-import { Paper, ScrollArea, Stack } from '@mantine/core'
+import { Box, Button, Group, Stack, Text } from '@mantine/core'
+
+import { useNoteContext } from '@features/note/model/context'
+import { IconPlus } from '@tabler/icons-react'
 
 export const NoteList = () => {
+  const { notes, activeNoteId, setActiveNoteId, addEmptyNote } =
+    useNoteContext()
+
+  const isEmpty = notes.length === 0
+
   return (
-    <ScrollArea w="33%" h="100vh" p="md">
-      <Stack>
-        <Paper withBorder p="sm" radius="md">
-          <h1>Note 1</h1>
-          <p>This is the content of note 1.</p>
-        </Paper>
-      </Stack>
-    </ScrollArea>
+    <Stack miw={300} maw={300} h="100vh" p="md">
+      <Group justify="space-between" mb="sm">
+        <Text fw={500}>Мои заметки</Text>
+        <Button variant="subtle" size="xs" onClick={addEmptyNote}>
+          <IconPlus size={16} />
+        </Button>
+      </Group>
+
+      {isEmpty ? (
+        <Stack align="center" h="100%">
+          <Text size="xl" fw={500} c="dimmed">
+            Создайте новую заметку.
+          </Text>
+        </Stack>
+      ) : (
+        <Stack gap="xs">
+          {notes.map((note) => (
+            <Box
+              key={note.id}
+              p="xs"
+              bg={note.id === activeNoteId ? 'gray' : ''}
+              onClick={() => setActiveNoteId(note.id)}
+              style={{
+                cursor: 'pointer',
+                borderRadius: 8,
+                wordBreak: 'break-word',
+              }}
+            >
+              <Text size="sm" fw={500} truncate>
+                {note.title || 'Без названия'}
+              </Text>
+              <Text size="xs" c="dimmed" lineClamp={1}>
+                {note.content || 'Нет содержимого'}
+              </Text>
+            </Box>
+          ))}
+        </Stack>
+      )}
+    </Stack>
   )
 }
