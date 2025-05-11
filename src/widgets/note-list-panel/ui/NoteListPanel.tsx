453310b60f4b1da6d63682e4c43e8b5694662cb6
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-05-11 23:11:40 +0300

    feat(dev): Добавить поиск по заметке.

--- src/widgets/note-list-panel/ui/NoteListPanel.tsx
+++ src/widgets/note-list-panel/ui/NoteListPanel.tsx
@@ -3,29 +3,34 @@ import { Button, Group, Loader, Stack, Text } from '@mantine/core'
 import { IconPlus } from '@tabler/icons-react'
 
 import { NoteList } from '@widgets/note-list/ui'
+import { SearchNotes } from '@widgets/search-notes/ui'
 
 export const NoteListPanel = () => {
   const { isLoadingNotes, addEmptyNote } = useNoteContext()
 
   return (
-      <Stack p="md" miw={300} maw={300} h="100vh" bg="#222222">
-        <Group justify="space-between" >
-          <Text fw={500}>Мои заметки</Text>
+    <Stack p="md" miw={300} maw={300} h="100vh" bg="#222222">
+      <Group justify="space-between">
+        <Text fw={500}>Мои заметки</Text>
+
+        <Group gap={5}>
+          <SearchNotes />
           <Button variant="subtle" size="xs" onClick={addEmptyNote}>
             <IconPlus size={16} />
           </Button>
         </Group>
+      </Group>
 
-        {isLoadingNotes ? (
-          <Group justify="center">
-            <Loader size="xs" />
-            <Text size="xs" c="dimmed">
-              Загружаем ваши заметки...
-            </Text>
-          </Group>
-        ) : (
-          <NoteList />
-        )}
+      {isLoadingNotes ? (
+        <Group justify="center">
+          <Loader size="xs" />
+          <Text size="xs" c="dimmed">
+            Загружаем ваши заметки...
+          </Text>
+        </Group>
+      ) : (
+        <NoteList />
+      )}
     </Stack>
   )
 }
